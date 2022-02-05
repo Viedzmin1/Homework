5eaf8736389fffe5ed6ed15022f0bff21679098e
viedzmin <rodion_obn@mail.ru>
2022-02-05 14:30:31 +0300

    Task_01 completed

--- Task_01.cs
+++ Task_01.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // 1. 1.	Вывести квадрат числа
+// // Вывел квадрат числа двумя способами:
+// 1. Просто умножил число само на себя
+// 2. Вывел через встроенную функцию Math.Pow
+
+int a=12;
+
+// int square = a*a;
+int square2 = (int) Math.Pow (a, 2);
+
+Console.WriteLine(square2);
+
